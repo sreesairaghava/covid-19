dcb5f26a640338beacc3411ed47ce2f3f24c0879
Sree Sai Raghava <raghava.rym@gmail.com>
2020-07-26 19:14:51 +0530

    Updates

---
+++ precache-manifest.d6f7574f7a9e6958d3170a5b552a6ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3649dce941d58ab026444256f13a21e",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "a4c4c9a9e754593879c4",
+    "url": "/covid-19/static/css/main.887d4b12.chunk.css"
+  },
+  {
+    "revision": "d259476de08c772677c8",
+    "url": "/covid-19/static/js/2.7fc6137f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.7fc6137f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c4c9a9e754593879c4",
+    "url": "/covid-19/static/js/main.db887cc5.chunk.js"
+  },
+  {
+    "revision": "2ca8e9579f423c215dc5",
+    "url": "/covid-19/static/js/runtime-main.a9cb5614.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
